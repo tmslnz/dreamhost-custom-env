830524d4bde04e700699547b89e8541451b559cd
Tommaso Lanza <t@tmslnz.com>
2010-05-15 16:01:56 +0100

    Fixed Berkeley DB linking, now Python compiles the bsddb module fine, no fuss. This version also sets up VirtualEnv and the wrapper script. So far so good :)

--- pyHost.sh
+++ pyHost.sh
@@ -32,11 +32,15 @@ tar -xzf tcl8.5.8-src.tar.gz
 wget http://prdownloads.sourceforge.net/tcl/tk8.5.8-src.tar.gz
 tar -xzf tk8.5.8-src.tar.gz
 wget http://pypi.python.org/packages/source/b/bsddb3/bsddb3-5.0.0.tar.gz
-tar -xvf bsddb3-5.0.0
+tar -xvf bsddb3-5.0.0.tar.gz
 wget http://python.org/ftp/python/2.6.5/Python-2.6.5.tgz
 tar -xzf Python-2.6.5.tgz
 wget http://download.oracle.com/berkeley-db/db-5.0.21.tar.gz
 tar -xzf db-5.0.21.tar.gz
+wget http://download.oracle.com/berkeley-db/db-4.8.30.tar.gz
+tar -xzf db-4.8.30.tar.gz
+wget http://download.oracle.com/berkeley-db/db-4.7.25.tar.gz
+tar -xzf db-4.7.25.tar.gz
 wget http://www.openssl.org/source/openssl-1.0.0.tar.gz
 tar -xzf openssl-1.0.0.tar.gz
 wget http://www.sqlite.org/sqlite-amalgamation-3.6.23.tar.gz
@@ -50,20 +54,40 @@ tar -xzf virtualenvwrapper-2.1.1.tar.gz
 
 # ##################################################################
 # Set temporary session paths for and variables for the GCC compiler
+# 
+# Specify the right version of Berkeley DB you want to use, see
+# below for DB install scripts.
 ####################################################################
-#unset LD_LIBRARY_PATH
-#unset LD_RUN_PATH
 
-export LD_LIBRARY_PATH=$HOME/opt/local/lib:$LD_LIBRARY_PATH
+# GCC
+export LD_LIBRARY_PATH=\
+$HOME/opt/local/lib:\
+$HOME/opt/db-4.7.25/lib:\
+$LD_LIBRARY_PATH
+
 export LD_RUN_PATH=$LD_LIBRARY_PATH
-export LDFLAGS="-L$HOME/opt/local/lib"
-export CPPFLAGS="-I$HOME/opt/local/include -I$HOME/opt/local/include/openssl -I$HOME/opt/local/include/readline"
+
+export LDFLAGS="\
+-L$HOME/opt/db-4.7.25/lib \
+-L$HOME/opt/local/lib"
+
+export CPPFLAGS="\
+-I$HOME/opt/local/include \
+-I$HOME/opt/local/include/openssl \
+-I$HOME/opt/db-4.7.25/include \
+-I$HOME/opt/local/include/readline"
+
 export CXXFLAGS=$CPPFLAGS
 export CFLAGS=$CPPFLAGS
+
+# Append Berkeley DB to EPREFIX. Used by Python setup.py
+export EPREFIX=$HOME/opt/db-4.7.25/lib:$EPREFIX
 
 # ###################
 # Compile and Install
 #####################
+
+# OpenSSL (required for haslib)
 cd openssl-1.0.0
 ./config --prefix=$HOME/opt/local --openssldir=$HOME/opt/local/openssl shared 
 make
@@ -77,56 +101,105 @@ ln -s libcrypto.so.1.0.0 libcrypto.so
 ln -s libssl.so.1.0.0 libssl.so
 cd ~/downloads
 
+# Readline
 cd readline-6.1
 ./configure --prefix=$HOME/opt/local
 make
 make install
 cd ..
 
+# Tcl/Tk
 cd tcl8.5.8/unix
 ./configure --prefix=$HOME/opt/local
 make
 make install
 cd ../..
-
 cd tk8.5.8/unix
 ./configure --prefix=$HOME/opt/local
 make
 make install
 cd ../..
+rm -f $HOME/opt/local/lib/libtcl8.so
+rm -f $HOME/opt/local/lib/libtcl.so
+rm -f $HOME/opt/local/lib/libtk8.so
+rm -f $HOME/opt/local/lib/libtk.so
+ln -s $HOME/opt/local/lib/libtcl8.5.so $HOME/opt/local/lib/libtcl8.so
+ln -s $HOME/opt/local/lib/libtcl8.5.so $HOME/opt/local/lib/libtcl.so
+ln -s $HOME/opt/local/lib/libtk8.5.so $HOME/opt/local/lib/libtk8.so
+ln -s $HOME/opt/local/lib/libtk8.5.so $HOME/opt/local/lib/libtk.so
 
+
+# Oracle Berkeley DB 5.0.x for Python 3
 cd db-5.0.21/build_unix
 ../dist/configure \
---prefix=$HOME/opt/local \
+--prefix=$HOME/opt/db-5.0.21 \
 --enable-tcl \
 --with-tcl=$HOME/opt/local/lib
 make
 make install
-# cd $HOME/opt/local/lib
-# cp libdb_tcl-5.0.so $HOME/opt/local/lib
-# cp libdb-5.0.so $HOME/opt/local/lib
-# cd $HOME/opt/local/lib
-# rm -f libdb_tcl-5.so
-# rm -f libdb_tcl.so
-# rm -f libdb-5.so
-# rm -f libdb.so
-# ln -s libdb_tcl-5.0.so libdb_tcl-5.so
-# ln -s libdb_tcl-5.0.so libdb_tcl.so
-# ln -s libdb-5.0.so libdb-5.so
-# ln -s libdb-5.0.so libdb.so
-# cd ~/downloads
+#cp $HOME/opt/db-5.0.21/lib/libdb_tcl-5.0.so $HOME/opt/local/lib
+#cp $HOME/opt/db-5.0.21/lib/libdb-5.0.so $HOME/opt/local/lib
+#rm -f $HOME/opt/local/lib/libdb_tcl-5.so
+#rm -f $HOME/opt/local/lib/libdb_tcl.so
+#rm -f $HOME/opt/local/lib/libdb-5.so
+#rm -f $HOME/opt/local/lib/libdb.so
+#ln -s $HOME/opt/local/lib/libdb_tcl-5.0.so $HOME/opt/local/lib/libdb_tcl-5.so
+#ln -s $HOME/opt/local/lib/libdb_tcl-5.0.so $HOME/opt/local/lib/libdb_tcl.so
+#ln -s $HOME/opt/local/lib/libdb-5.0.so $HOME/opt/local/lib/libdb-5.so
+#ln -s $HOME/opt/local/lib/libdb-5.0.so $HOME/opt/local/lib/libdb.so
 cd ../..
 
+# Oracle Berkeley DB 4.8.x fro Python 2.7.x
+cd db-4.8.30/build_unix
+../dist/configure \
+--prefix=$HOME/opt/db-4.8.30 \
+--enable-tcl \
+--with-tcl=$HOME/opt/local/lib
+make
+make install
+#cp $HOME/opt/db-4.8.30/lib/libdb_tcl-4.8.so $HOME/opt/local/lib
+#cp $HOME/opt/db-4.8.30/lib/libdb-4.8.so $HOME/opt/local/lib
+#rm -f $HOME/opt/local/lib/libdb_tcl-4.so
+#rm -f $HOME/opt/local/lib/libdb_tcl.so
+#rm -f $HOME/opt/local/lib/libdb-4.so
+#rm -f $HOME/opt/local/lib/libdb.so
+#ln -s $HOME/opt/local/lib/libdb_tcl-4.8.so $HOME/opt/local/lib/libdb_tcl-4.so
+#ln -s $HOME/opt/local/lib/libdb_tcl-4.8.so $HOME/opt/local/lib/libdb_tcl.so
+#ln -s $HOME/opt/local/lib/libdb-4.8.so $HOME/opt/local/lib/libdb-4.so
+#ln -s $HOME/opt/local/lib/libdb-4.8.so $HOME/opt/local/lib/libdb.so
+cd ../..
+
+# Oracle Berkeley DB 4.7.x fro Python 2.6.x
+cd db-4.7.25/build_unix
+../dist/configure \
+--prefix=$HOME/opt/db-4.7.25 \
+--enable-tcl \
+--with-tcl=$HOME/opt/local/lib
+make
+make install
+cp $HOME/opt/db-4.7.25/lib/libdb_tcl-4.7.so $HOME/opt/local/lib
+cp $HOME/opt/db-4.7.25/lib/libdb-4.7.so $HOME/opt/local/lib
+rm -f $HOME/opt/local/lib/libdb_tcl-4.so
+rm -f $HOME/opt/local/lib/libdb_tcl.so
+rm -f $HOME/opt/local/lib/libdb-4.so
+rm -f $HOME/opt/local/lib/libdb.so
+ln -s $HOME/opt/local/lib/libdb_tcl-4.7.so $HOME/opt/local/lib/libdb_tcl-4.so
+ln -s $HOME/opt/local/lib/libdb_tcl-4.7.so $HOME/opt/local/lib/libdb_tcl.so
+ln -s $HOME/opt/local/lib/libdb-4.7.so $HOME/opt/local/lib/libdb-4.so
+ln -s $HOME/opt/local/lib/libdb-4.7.so $HOME/opt/local/lib/libdb.so
+cd ../..
+
+# Bzip (required by hgweb)
 cd bzip2-1.0.5
 make -f Makefile-libbz2_so
 make
 make install PREFIX=$HOME/opt/local
 cp libbz2.so.1.0.4 $HOME/opt/local/lib
-cd $HOME/opt/local/lib
-rm -f libbz2.so.1.0
-ln -s libbz2.so.1.0.4 libbz2.so.1.0
-cd ~/downloads
+rm -f $HOME/opt/local/lib/libbz2.so.1.0
+ln -s $HOME/opt/local/lib/libbz2.so.1.0.4 $HOME/opt/local/lib/libbz2.so.1.0
+cd ..
 
+# SQLite
 cd sqlite-3.6.23
 ./configure --prefix=$HOME/opt/local
 make
@@ -134,16 +207,23 @@ make install
 cd ..
 
 cd Python-2.6.5
-./configure --prefix=$HOME/opt/local
+./configure --prefix=$HOME/opt/Python-2.6.5
 make
 make install
 cd ..
 
 cd mercurial-1.5.2
 make install PREFIX=$HOME/opt/local
-#python setup.py install --home=$HOME/opt/local
 cd ..
 
-# ~/opt/Python-2.6.5/bin/python2.6 virtualenv-1.4.3/virtualenv.py $HOME/opt/local
-# easy_install virtualenv
-# cd ..
+cd virtualenv-1.4.8
+# May need to use 'python2.5' instead of 'python' here
+# as the script may require a *system* installation of python
+python virtualenv.py $HOME/opt/local
+easy_install virtualenv
+cd ..
+
+cp virtualenvwrapper-2.1.1/virtualenvwrapper.sh ~/opt/
+
+cd ~
+mkdir $HOME/.virtualenvs
